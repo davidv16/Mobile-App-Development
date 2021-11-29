f45d06d2af793db1f21230138674db0b527c7a23
davidv16 <dabbithekking@gmail.com>
2021-11-29 14:25:23 +0000

    components added to the main view

--- Assignment II - The Contactor/Contactor/src/views/Contacts/index.tsx
+++ Assignment II - The Contactor/Contactor/src/views/Contacts/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import AddContact from '../../components/AddContact';
 import ContactList from '../../components/ContactList';
+import Search from '../../components/Search';
 
 const Contacts = () => {
     return (
         <View>
             Contacts View
+            <Search />
             <ContactList />
+            <AddContact />
         </View>
     );
 };
