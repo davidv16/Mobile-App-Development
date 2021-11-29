4959d6e199332699e391b2d61231aee9a15c91d6
davidv16 <dabbithekking@gmail.com>
2021-11-29 12:56:27 +0000

    project 2 setup for typescript

--- Assignment II - The Contactor/Contactor/.eslintrc.js
+++ Assignment II - The Contactor/Contactor/.eslintrc.js
@@ -1,24 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
   },
   extends: [
-    'plugin:react/recommended',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'airbnb',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
   rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: false }],
+    'react/style-prop-object': [1, { allow: ['string'] }],
+    'react/function-component-definition': [2, { unnamedComponents: 'function-expression' }],
   },
 };
