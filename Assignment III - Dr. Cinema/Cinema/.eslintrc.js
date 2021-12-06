d9d32a6c34bf3a1cb3c872eb8cf29f3bad1c000a
davidv16 <dabbithekking@gmail.com>
2021-12-06 09:25:30 +0000

    assignent 3 setup

---
+++ Assignment III - Dr. Cinema/Cinema/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'airbnb',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: false }],
+    'react/style-prop-object': [1, { allow: ['string'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'linebreak-style': 'off',
+  },
+};
