4a3545a7854dbf5654eb4afc9a29b139198a007a
Gauti <gudmundurgj20@ru.is>
2021-12-08 09:34:32 +0000

    redux works in upcoming movies

--- Assignment III - Dr. Cinema/Cinema/src/actions/upcomingActions.ts
+++ Assignment III - Dr. Cinema/Cinema/src/actions/upcomingActions.ts
@@ -1,9 +1,11 @@
 import { GET_UPCOMING_MOVIES } from "../constants";
 import { getUpcomingMovies } from "../services";
+import IUpcomingMovie from '../models/IUpcomingMovie'
+import { Dispatch } from "redux";
 
-export const getUpcoming = () => async dispatch => {
+export const getUpcoming = () => async (dispatch: Dispatch) => {
     const upcomingMovies = await getUpcomingMovies();
-    // console.log('inside async action creator: ' + upcomingMovies[0].title);
+    // console.log('inside async action creator: ' + ));
     dispatch(getUpcomingMoviesSuccess(upcomingMovies))
 };
 
--- Assignment III - Dr. Cinema/Cinema/src/constants/index.ts
+++ Assignment III - Dr. Cinema/Cinema/src/constants/index.ts
@@ -1,1 +1,2 @@
 export const GET_UPCOMING_MOVIES = "GET-UPCOMING_MOVIES"
+export const GET_CINEMAS = "GET-CINEMAS"
